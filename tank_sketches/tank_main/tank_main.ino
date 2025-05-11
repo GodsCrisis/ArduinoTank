70cd97bb6e93cab53f37571756d182508acd7026
igorciz777 <igorciz777@gmail.com>
2025-05-11 15:37:50 +0200

    neopixel i prosta animacja swiatla wystrzalu

--- tank_sketches/tank_main/tank_main.ino
+++ tank_sketches/tank_main/tank_main.ino
@@ -4,6 +4,7 @@
 #include <printf.h>
 #include <stdint.h>
 #include <Servo.h>
+#include <Adafruit_NeoPixel.h>
 
 // Piny
 #define SERVO_PWM 9
@@ -13,78 +14,147 @@
 #define MOTOR_B_L 12
 #define MOTOR_B_R 13
 #define MOTOR_B_PWM 11
-// #define DFPLAYER_IO1 13
+#define NEOPIXEL_PWM 6
+
+// NeoPixel
+#define PIXEL_COUNT 4
 
 struct __attribute__((packed)) radio_payload_t {
-  uint8_t  pos;    // 0…255
+  uint8_t pos;     // 0…255
   uint16_t slide;  // 0…1023
   uint16_t rot;    // 0…1023
-  uint8_t  sw;     // 0 or 1
+  uint8_t sw;      // 0 or 1
 };
 
-// CE = D7, CSN = D8 (dowolne piny cyfrowe)
 RF24 radio(7, 8);
 Servo servo;
+Adafruit_NeoPixel neopixel(PIXEL_COUNT, NEOPIXEL_PWM, NEO_GRBW + NEO_KHZ800);
 
-void setup()
-{
+// Stan animacji dla neopixela
+enum AnimationState {
+  ANIM_IDLE,
+  ANIM_EXPLOSION
+};
+
+void neopixelSetColor(uint8_t r, uint8_t g, uint8_t b, uint8_t w = 0) {
+  for (int i = 0; i < PIXEL_COUNT; i++) {
+    neopixel.setPixelColor(i, neopixel.Color(r, g, b, w));
+  }
+}
+
+AnimationState animState = ANIM_IDLE;
+unsigned long animStartTime = 0;
+uint8_t animPhase = 0;
+
+void setup() {
   Serial.begin(9600);
-  //while (!Serial);  // Czekaj na otwarcie monitora szeregowego
-  printf_begin();   // Inicjalizacja printf dla nrf24
-  
+  printf_begin();
+
   /*RADIO START*/
   const byte address[] = "1Tank";
   bool result = radio.begin();
-  radio.setChannel(76);               // Ten sam kanał co w Pico
-  radio.setPALevel(RF24_PA_LOW);      // Mniejsze zakłócenia
-  radio.setDataRate(RF24_250KBPS);    // Taki sam jak u nadajnika
-  radio.openReadingPipe(0, address);  // Adres odbiorczy
-  radio.startListening();             // Przejście w tryb odbioru
-  radio.printDetails();               // Wyświetlenie szczegółów połączenia
+  radio.setChannel(76);
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setDataRate(RF24_250KBPS);
+  radio.openReadingPipe(0, address);
+  radio.startListening();
+  radio.printDetails();
   /*RADIO END*/
 
   /*MOTORS START*/
   pinMode(SERVO_PWM, OUTPUT);
   servo.attach(SERVO_PWM);
   servo.write(90);
-  digitalWrite(4,HIGH);
-  //digitalWrite(2,LOW);
+  digitalWrite(4, HIGH);
   /*MOTORS END*/
 
-  /*DFPLAYER*/
-  //digitalWrite(DFPLAYER_IO1,LOW);
+  /*NEOPIXEL START*/
+  pinMode(NEOPIXEL_PWM, INPUT_PULLUP);
+  neopixel.begin();
+  neopixel.clear();
+  neopixelSetColor(0,0,0); // TODO: na poczatku jest zielone jedno zapalone i nie wiem dlaczego 
+  neopixel.show();
+  /*NEOPIXEL END*/
 }
 
-void loop()
-{
-  if(radio.available())
-  {
+void updateExplosionAnimation() {
+  if (animState != ANIM_EXPLOSION) return;
+
+  unsigned long currentTime = millis();
+  unsigned long elapsed = currentTime - animStartTime;
+
+  // Explosion animation sequence with corrected colors
+  if (elapsed < 50) {
+    neopixelSetColor(255, 165, 0);  // Orange
+    neopixel.show();
+  } else if (elapsed < 100) {
+    neopixelSetColor(255, 100, 0);  // Darker orange
+    neopixel.show();
+  } else if (elapsed < 150) {
+    neopixelSetColor(255, 50, 0);  // Red-orange
+    neopixel.show();
+  } else if (elapsed < 200) {
+    neopixelSetColor(255, 0, 0);  // Bright red
+    neopixel.show();
+  } else if (elapsed < 250) {
+    neopixelSetColor(150, 0, 0);  // Dark red
+    neopixel.show();
+  } else if (elapsed < 300) {
+    neopixelSetColor(50, 0, 0);  // Very dark red
+    neopixel.show();
+  } else {
+    // koniec animacji
+    neopixel.clear();
+    neopixel.show();
+    animState = ANIM_IDLE;
+  }
+}
+
+void startExplosionAnimation() {
+  animState = ANIM_EXPLOSION;
+  animStartTime = millis();
+}
+
+void loop() {
+  // Update NeoPixel
+  updateExplosionAnimation();
+
+  // Radio komunikacja
+  if (radio.available()) {
     radio_payload_t payload;
     radio.read(&payload, sizeof(payload));
     printf("pos=%u  slide=%u  rot=%u  btn=%u\n",
-                payload.pos, payload.slide, payload.rot, payload.sw);
-    radio.flush_rx(); // Opróżnienie bufora odbiorczego
-    servo.write(map(payload.pos,96,160,0,180));
-    int mapped_slider = map(payload.slide,0,1023,-255,255);
-    int mapped_rot = map(payload.rot,0,1023,-127,127);
-    if(mapped_slider >= 0)
-    {
-      analogWrite(MOTOR_A_PWM,min(mapped_slider - mapped_rot,255));
-      analogWrite(MOTOR_B_PWM,min(mapped_slider + mapped_rot,255));
-      digitalWrite(MOTOR_A_L,HIGH);
-      digitalWrite(MOTOR_A_R,LOW);
-      digitalWrite(MOTOR_B_L,HIGH);
-      digitalWrite(MOTOR_B_R,LOW);
+           payload.pos, payload.slide, payload.rot, payload.sw);
+    radio.flush_rx();
+
+    // Serwo
+    servo.write(map(payload.pos, 96, 160, 0, 180));
+
+    // Silniki
+    int mapped_slider = map(payload.slide, 0, 1023, -255, 255);
+    int mapped_rot = map(payload.rot, 0, 1023, -127, 127);
+
+    if (mapped_slider >= 0) {
+      analogWrite(MOTOR_A_PWM, min(mapped_slider - mapped_rot, 255));
+      analogWrite(MOTOR_B_PWM, min(mapped_slider + mapped_rot, 255));
+      digitalWrite(MOTOR_A_L, HIGH);
+      digitalWrite(MOTOR_A_R, LOW);
+      digitalWrite(MOTOR_B_L, HIGH);
+      digitalWrite(MOTOR_B_R, LOW);
+    } else {
+      analogWrite(MOTOR_A_PWM, min(-mapped_slider - mapped_rot, 255));
+      analogWrite(MOTOR_B_PWM, min(-mapped_slider + mapped_rot, 255));
+      digitalWrite(MOTOR_A_R, HIGH);
+      digitalWrite(MOTOR_A_L, LOW);
+      digitalWrite(MOTOR_B_R, HIGH);
+      digitalWrite(MOTOR_B_L, LOW);
     }
-    else
-    {
-      analogWrite(MOTOR_A_PWM,min(-mapped_slider - mapped_rot,255));
-      analogWrite(MOTOR_B_PWM,min(-mapped_slider + mapped_rot,255));
-      digitalWrite(MOTOR_A_R,HIGH);
-      digitalWrite(MOTOR_A_L,LOW);
-      digitalWrite(MOTOR_B_R,HIGH);
-      digitalWrite(MOTOR_B_L,LOW);
+
+    // Odtwórz animację na przycisk
+    if (payload.sw == 1 && animState == ANIM_IDLE) {
+      startExplosionAnimation();
     }
   }
+
   delay(10);
 }
